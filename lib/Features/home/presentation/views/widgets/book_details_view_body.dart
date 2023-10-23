eb5cd4e38d05a870cb0dd3c58242dcbad404c74a
Omar_Ameer <omar.ameer244@gmail.com>
2023-10-23 20:54:52 +0300

    BookDetailsView Done.

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -4,6 +4,7 @@ import 'package:bookly_app/Features/home/presentation/views/widgets/book_rating.
 import 'package:bookly_app/Features/home/presentation/views/widgets/books_actions.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_image.dart.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -16,7 +17,7 @@ class BookDetailsViewBody extends StatelessWidget {
         const CustomBookDetailsAppBar(),
         Padding(
           padding: EdgeInsets.symmetric(
-            horizontal: Responsive.screenWidth(context) * 0.26,
+            horizontal: Responsive.screenWidth(context) * 0.28,
           ),
           child: const CustomBookImage(),
         ),
@@ -51,6 +52,30 @@ class BookDetailsViewBody extends StatelessWidget {
           height: 37,
         ),
         const BooksActions(),
+        const SizedBox(
+          height: 43,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'You may also like',
+              style: Styles.montserratSemiBold.copyWith(
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        const Expanded(
+          child: SimilarBooksListView(),
+        ),
+        const SizedBox(
+          height: 27,
+        ),
       ],
     );
   }
