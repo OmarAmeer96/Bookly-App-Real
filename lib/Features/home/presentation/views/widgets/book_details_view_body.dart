d6e9a57607ac5ebd9d090619892d01c928cc276f
Omar_Ameer <omar.ameer244@gmail.com>
2023-10-23 21:28:28 +0300

    Refactoring UI Code Done.

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,10 +1,6 @@
-import 'package:bookly_app/Core/utils/responsive.dart';
-import 'package:bookly_app/Core/utils/styles.dart';
-import 'package:bookly_app/Features/home/presentation/views/widgets/book_rating.dart';
-import 'package:bookly_app/Features/home/presentation/views/widgets/books_actions.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/book_details_section.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
-import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_image.dart.dart';
-import 'package:bookly_app/Features/home/presentation/views/widgets/similar_books_list_view.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -12,76 +8,24 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
+    return const CustomScrollView(
       slivers: [
         SliverFillRemaining(
           hasScrollBody: false,
           child: Column(
             children: [
-              const CustomBookDetailsAppBar(),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: Responsive.screenWidth(context) * 0.28,
-                ),
-                child: const CustomBookImage(),
-              ),
-              const SizedBox(
-                height: 43,
-              ),
-              Text(
-                'King Kong Book',
-                style: Styles.gtSectraFineRegular.copyWith(
-                  fontSize: 34,
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              Opacity(
-                opacity: 0.6,
-                child: Text(
-                  'Omar Ameer',
-                  style: Styles.montserratMedium.copyWith(
-                    fontSize: 22,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              const BookRating(
-                mainAxisAlignment: MainAxisAlignment.center,
-              ),
-              const SizedBox(
-                height: 37,
-              ),
-              const BooksActions(),
-              const Expanded(
+              CustomBookDetailsAppBar(),
+              BookDetailsSection(),
+              
+              
+              Expanded(
                 child: SizedBox(
                   height: 43,
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'You may also like',
-                    style: Styles.montserratSemiBold.copyWith(
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
+              SimilarBooksSection(),
               SizedBox(
-                height: Responsive.screenHeight(context) * 0.15,
-                child: const SimilarBooksListView(),
-              ),
-              const SizedBox(
-                height: 27,
+                height: 25,
               ),
             ],
           ),
