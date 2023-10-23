87c59e353d221060e7a7f840adced0538a9d6d2a
Omar_Ameer <omar.ameer244@gmail.com>
2023-10-23 15:12:57 +0300

    Home View Done.

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,5 @@
 import 'package:bookly_app/Core/utils/styles.dart';
-import 'package:bookly_app/Features/home/presentation/views/widgets/best_seller_list_view.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -9,11 +9,12 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
-      physics: BouncingScrollPhysics(),
+    return CustomScrollView(
+      physics: const BouncingScrollPhysics(),
       slivers: [
-        SliverToBoxAdapter(
+        const SliverToBoxAdapter(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Padding(
                 padding: EdgeInsets.only(bottom: 16),
@@ -23,13 +24,12 @@ class HomeViewBody extends StatelessWidget {
               SizedBox(
                 height: 40,
               ),
-              Row(
-                children: [
-                  Text(
-                    "Best Seller",
-                    style: Styles.montserratSemiBold,
-                  ),
-                ],
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  "Best Seller",
+                  style: Styles.montserratSemiBold,
+                ),
               ),
               SizedBox(
                 height: 30,
@@ -37,9 +37,52 @@ class HomeViewBody extends StatelessWidget {
             ],
           ),
         ),
-        SliverToBoxAdapter(
-          child: BestSellerListView(),
+        /*
+        THE SLIVER LIST WAS LIKE THIS:
+        SliverFillRemaining(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: BestSellerListView(),
+          ),
         ),
+
+        =======================================================
+
+        AND THIS IS THE BEST SELLER LIST VIEW FILE:
+        class BestSellerListView extends StatelessWidget {
+          const BestSellerListView({super.key});
+          @override
+          Widget build(BuildContext context) {
+            return SizedBox(
+              child: ListView.builder(
+                padding: EdgeInsets.zero,
+                itemCount: 10,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.only(bottom: 20),
+                    child: BestSellerListViewItem(),
+                  );
+                },
+              ),
+            );
+          }
+        }
+        */
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              return const Padding(
+                padding: EdgeInsets.only(bottom: 20),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: BestSellerListViewItem(),
+                ),
+              );
+            },
+            childCount: 10,
+          ),
+        )
       ],
     );
   }
