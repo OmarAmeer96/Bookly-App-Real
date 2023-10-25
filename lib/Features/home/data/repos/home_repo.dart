afaaf823bdfa4f67123190832cd5bdfdbd54fb65
Omar_Ameer <omar.ameer244@gmail.com>
2023-10-25 23:08:22 +0300

    Create Home Repo.

---
+++ lib/Features/home/data/repos/home_repo.dart
@@ -0,0 +1,7 @@
+// REPOSITORY PATTERN
+// This class indicates what's this feature (home feature) is going to do.
+
+abstract class HomeRepo {
+  fetchBestSellerBooks();
+  fetchFeatureedBooks();
+}
