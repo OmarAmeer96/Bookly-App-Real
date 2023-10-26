7f9c58ae2133fe29f12943dedc6b16cb0dbda56f
Omar_Ameer <omar.ameer244@gmail.com>
2023-10-26 15:50:42 +0300

    Create Home Repo Impl Class.

---
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,18 @@
+import 'package:bookly_app/Core/errors/failures.dart';
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo{
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeatureedBooks() {
+    // TODO: implement fetchFeatureedBooks
+    throw UnimplementedError();
+  }
+}
