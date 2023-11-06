8e0db3ccfce199064fb260df17aae08208df0953
Omar_Ameer <omar.ameer244@gmail.com>
2023-11-06 19:05:53 +0200

    Error Handled.

--- lib/Features/home/presentation/views/widgets/custom_book_image.dart.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_image.dart.dart
@@ -16,40 +16,40 @@ class CustomBookImage extends StatelessWidget {
       child: ClipRRect(
         borderRadius: BorderRadiusDirectional.circular(16),
         child: AspectRatio(
-            aspectRatio: 150 / 224,
-            child: CachedNetworkImage(
-              imageUrl: imageUrl,
-              fit: BoxFit.fill,
-              errorWidget: (context, url, error) => Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      Icons.error_outline,
-                      size: 24,
-                    ),
-                    const SizedBox(
-                      height: 16,
-                    ),
-                    Text(
-                      "Couldn't Load the Book 😢",
-                      textAlign: TextAlign.center,
-                      style: Styles.montserratSemiBold.copyWith(fontSize: 16),
-                    ),
-                  ],
-                ),
+          aspectRatio: 150 / 224,
+          child: CachedNetworkImage(
+            imageUrl: imageUrl,
+            fit: BoxFit.fill,
+            errorWidget: (context, url, error) => Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.error_outline,
+                    size: 24,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    "Couldn't Load the Book 😢",
+                    textAlign: TextAlign.center,
+                    style: Styles.montserratSemiBold.copyWith(fontSize: 16),
+                  ),
+                ],
               ),
-            )
-            // Container(
-            //   decoration: BoxDecoration(
-            //     image: DecorationImage(
-            //       image: NetworkImage(imageUrl),
-            //       fit: BoxFit.fill,
-            //     ),
-            //     borderRadius: BorderRadius.circular(18),
-            //   ),
-            // ),
             ),
+          ),
+          // Container(
+          //   decoration: BoxDecoration(
+          //     image: DecorationImage(
+          //       image: NetworkImage(imageUrl),
+          //       fit: BoxFit.fill,
+          //     ),
+          //     borderRadius: BorderRadius.circular(18),
+          //   ),
+          // ),
+        ),
       ),
     );
   }
