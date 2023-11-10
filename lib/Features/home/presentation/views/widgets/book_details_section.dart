a88b41db471f5969ba26518397eedc9f416950db
Omar_Ameer <omar.ameer244@gmail.com>
2023-11-10 16:46:08 +0200

    Added the Img, Title, Author, Rating to BookDetailes View.

--- lib/Features/home/presentation/views/widgets/book_details_section.dart
+++ lib/Features/home/presentation/views/widgets/book_details_section.dart
@@ -1,12 +1,15 @@
 import 'package:bookly_app/Core/utils/responsive.dart';
 import 'package:bookly_app/Core/utils/styles.dart';
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/books_actions.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_image.dart.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsSection extends StatelessWidget {
-  const BookDetailsSection({super.key});
+  const BookDetailsSection({super.key, required this.bookModel});
+
+  final BookModel bookModel;
 
   @override
   Widget build(BuildContext context) {
@@ -16,18 +19,23 @@ class BookDetailsSection extends StatelessWidget {
           padding: EdgeInsets.symmetric(
             horizontal: Responsive.screenWidth(context) * 0.28,
           ),
-          child: const CustomBookImage(
-            imageUrl:
-                "https://web-strapi.mrmilu.com/uploads/flutter_logo_470e9f7491.png",
+          child: CustomBookImage(
+            imageUrl: bookModel.volumeInfo.imageLinks!.thumbnail,
           ),
         ),
         const SizedBox(
           height: 43,
         ),
-        Text(
-          'King Kong Book',
-          style: Styles.gtSectraFineRegular.copyWith(
-            fontSize: 34,
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Text(
+            bookModel.volumeInfo.title,
+            textAlign: TextAlign.center,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+            style: Styles.gtSectraFineRegular.copyWith(
+              fontSize: 34,
+            ),
           ),
         ),
         const SizedBox(
@@ -35,19 +43,25 @@ class BookDetailsSection extends StatelessWidget {
         ),
         Opacity(
           opacity: 0.6,
-          child: Text(
-            'Omar Ameer',
-            style: Styles.montserratMedium.copyWith(
-              fontSize: 22,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              bookModel.volumeInfo.publisher,
+              textAlign: TextAlign.center,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: Styles.montserratMedium.copyWith(
+                fontSize: 22,
+              ),
             ),
           ),
         ),
         const SizedBox(
           height: 16,
         ),
-        const BookRating(
-          rating: 4,
-          count: 5002,
+        BookRating(
+          rating: bookModel.volumeInfo.averageRating?.round() ?? 0,
+          count: bookModel.volumeInfo.ratingsCount ?? 0,
           mainAxisAlignment: MainAxisAlignment.center,
         ),
         const SizedBox(
--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,28 +1,33 @@
+import 'package:bookly_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/book_details_section.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
-  const BookDetailsViewBody({super.key});
+  const BookDetailsViewBody({super.key, required this.bookModel});
+
+  final BookModel bookModel;
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
+    return CustomScrollView(
       slivers: [
         SliverFillRemaining(
           hasScrollBody: false,
           child: Column(
             children: [
-              CustomBookDetailsAppBar(),
-              BookDetailsSection(),
-              Expanded(
+              const CustomBookDetailsAppBar(),
+              BookDetailsSection(
+                bookModel: bookModel,
+              ),
+              const Expanded(
                 child: SizedBox(
                   height: 43,
                 ),
               ),
-              SimilarBooksSection(),
-              SizedBox(
+              const SimilarBooksSection(),
+              const SizedBox(
                 height: 25,
               ),
             ],
